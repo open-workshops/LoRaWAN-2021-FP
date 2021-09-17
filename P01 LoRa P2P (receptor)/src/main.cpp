4589f42695f7793d23f96a86edceb48587de6b4b
TCRobotics <alex.tc.robotics@gmail.com>
2021-09-17 19:36:02 +0200

    Style consensus

--- P01 LoRa P2P (receptor)/src/main.cpp
+++ P01 LoRa P2P (receptor)/src/main.cpp
@@ -1,31 +1,30 @@
+/*******************************************************************************
+ P1: Práctica de LoRa P2P (Receptor)
+ 
+ Este ejemplo permite recibir las tramas de LoRa enviadas por el transmisor.
+ ******************************************************************************/
 #include <Arduino.h>
-/*
-  P1: Práctica de LoRa P2P (Receptor)
-  
-  Este ejemplo permite recibir ñas tramas de LoRa enviadas por el transmisor.
-*/
-
 #include <RadioLib.h>
 
-// SX1278 has the following connections:
-// NSS pin:   SS = 18
-// DIO0 pin:  DIO0 = 26
-// RESET pin: RST_LoRa = 14
-// DIO1 pin:  DIO1 = 35
-SX1278 radio = new Module(SS, DIO0, RST_LoRa, DIO1);
+// PINOUT SX1278
+// LORA MODULE NSS pin   SS       = 18
+// LORA MODULE DIO0 pin  DIO0     = 26
+// LORA MODULE RESET pin RST_LoRa = 14
+// LORA MODULE DIO1 pin  DIO1     = 35
+SX1278 radio = new Module(NSS_PIN, DIO0, RST_LoRa, DIO1);
 
 void setup()
 {
   Serial.begin(115200);
-  delay(4000); // esperamos a que el minitor serial este listo
+  delay(4000); // esperamos a que el monitor serial este listo
   
   // initialize SX1278 with default settings
   Serial.print("Inicializando... ");
 
-  // Freq: 434.0
-  // Bandwidth: 125
-  // Spreading factor: 7
-  int state = radio.begin(434.0, 125, 7);
+  // Freq: 865.0 - 870.0 MHz
+  // Bandwidth: 125 MHz
+  // Spreading factor: SF7
+  int state = radio.begin(868.1, 125, 7);
 
   if (state == ERR_NONE)
   {
@@ -37,39 +36,35 @@ void setup()
     Serial.println(state);
     while (true);
   }
-
-  
-  Serial.print(F("Escuchando a la espera de una transmisión ... "));
+  Serial.print("Escuchando a la espera de una transmisión ... ");
 }
 
 void loop() {
-  
-
   String str;
   int state = radio.receive(str);
 
   if (state == ERR_NONE)
   {
-    Serial.println(F("Recibido!"));
+    Serial.println("Recibido!");
 
-    Serial.print(F("Data:\t\t\t"));
+    Serial.print("Data:\t\t\t");
     Serial.println(str);
 
-    Serial.print(F("RSSI:\t\t\t"));
+    Serial.print("RSSI:\t\t\t");
     Serial.print(radio.getRSSI());
     Serial.println(F(" dBm"));
 
-    Serial.print(F("SNR:\t\t\t"));
+    Serial.print("SNR:\t\t\t");
     Serial.print(radio.getSNR());
     Serial.println(F(" dB"));
 
-    Serial.print(F("Escuchando a la espera de una transmisión ... "));
+    Serial.print("Escuchando a la espera de una transmisión ... ");
   } 
   else if (state != ERR_RX_TIMEOUT)
   {
-    Serial.print(F("Error, codigo: "));
+    Serial.print("Error, codigo: "));
     Serial.println(state);
 
-    Serial.print(F("Escuchando a la espera de una transmisión ... "));
+    Serial.print("Escuchando a la espera de una transmisión ... ");
   }
 }
