868b405cf91c0bc0a21bb5d4b2b3eb93c7fe46f8
4m1g0 <dev.4m1g0@gmail.com>
2021-10-20 20:32:42 +0200

    Add door sensor and led to OTAA exercise and comments to ABP exercise

--- P03 LoRaWAN ABP/src/main.cpp
+++ P03 LoRaWAN ABP/src/main.cpp
@@ -1,42 +1,60 @@
-#include <TTN_esp32.h>
-
 /***************************************************************************
- *  Go to your TTN console register a device then the copy fields
- *  and replace the CHANGE_ME strings below
- ****************************************************************************/
-const char* devAddr = "CHANGE_ME"; // Change to TTN Device Address
-const char* nwkSKey = "CHANGE_ME"; // Change to TTN Network Session Key
-const char* appSKey = "CHANGE_ME"; // Change to TTN Application Session Key
+  P03: Práctica de LoRaWAN con ABP
+  
+  Este ejemplo permite enviar uplinks con activación por ABP
+  - Cada 15 segundos se envía un uplink LoRaWAN con un hola mundo
+  - Tras cada uplink se escucha por si llega un downlink
 
-TTN_esp32 ttn ;
+  Crea un dispositivo en TTN y rellena los campos de abajo con
+  las claves correspondientes
 
-void message(const uint8_t* payload, size_t size, int rssi)
-{
-    Serial.println("-- MESSAGE");
-    Serial.print("Received " + String(size) + " bytes RSSI= " + String(rssi) + "dB");
+    # PAYLOAD FORMATER para TTN #
 
-    for (int i = 0; i < size; i++)
-    {
-        Serial.print(" " + String(payload[i]));
+    function Decoder(bytes, port) {
+      // Decode plain text; for testing only 
+      return {
+        myTestValue: String.fromCharCode.apply(null, bytes)
+      };
     }
 
-    Serial.println();
+ ****************************************************************************/
+#include <Arduino.h>
+#include <TTN_esp32.h>
+
+const char *devAddr = "XXXXXXX";                          // Rellena con el Device Address de TTN
+const char *nwkSKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Rellena con la Network Session Key de TTN
+const char *appSKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Rellena con la Application Session Key de TTN
+
+TTN_esp32 ttn;
+
+void message(const uint8_t *payload, size_t size, int rssi)
+{
+  Serial.println("-- NUEVO DOWNLINK");
+  Serial.println("Recibidos " + String(size) + " bytes RSSI=" + String(rssi) + "db");
+
+  for (int i = 0; i < size; i++)
+  {
+    Serial.print(" " + String(payload[i]));
+  }
+
+  Serial.println();
 }
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("Starting");
-    ttn.begin();
-    ttn.onMessage(message); // declare callback function when is downlink from server
-    ttn.personalize(devAddr, nwkSKey, appSKey);
-    ttn.showStatus();
+  Serial.begin(115200);
+  Serial.println("Iniciando");
+
+  ttn.begin();
+  ttn.onMessage(message); // declaramos la función de callback para manejar los downlinks desde el servidor
+  ttn.personalize(devAddr, nwkSKey, appSKey);
+  ttn.showStatus();
 }
 
 void loop()
 {
-    uint8_t* payload = (uint8_t*) "hola";
-    ttn.sendBytes(payload, 5);
-    Serial.println("Enviando uplink...");
-    delay(60000);
+  uint8_t *payload = (uint8_t *)"hola";
+  ttn.sendBytes(payload, 4);
+  Serial.println("Enviando uplink...");
+  delay(15000);
 }
