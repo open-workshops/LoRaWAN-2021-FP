693e35487065b0b1b61c3cd250a301de81224020
4m1g0 <dev.4m1g0@gmail.com>
2021-10-21 20:26:16 +0200

    Add oled to exercies P05a and include schematic diagram

--- P05a Sensor temperatura Hola mundo/src/main.cpp
+++ P05a Sensor temperatura Hola mundo/src/main.cpp
@@ -2,8 +2,9 @@
   P05a: Hola mundo sensor de temperatura y humedad
   
   Este ejemplo permite leer datos del sensor de temperatura y humedad SHT30
+  - Se inicializa la pantalla OLED
   - La placa lee el estado de un sensor SHT30 (temperatura y humedad)
-  - Los valores de temperatura y humedad se imprimen por serial
+  - Los valores de temperatura y humedad se imprimen por serial y en la pantalla
 
   Conexiones:
     Sensor de temperatura     Placa Heltec
@@ -16,12 +17,31 @@
 
 #include <Arduino.h>
 #include <WEMOS_SHT3X.h>
+#include <SSD1306Wire.h>
 
 SHT3X sht30(0x44);
 
+SSD1306Wire display(0x3c, SDA_OLED, SCL_OLED); 
+
 void setup() {
   Serial.begin(115200);
-  delay(1000);
+
+  // Reseteamos la OLED
+  pinMode(RST_OLED,OUTPUT);
+  digitalWrite(RST_OLED, LOW);     
+  delay(50);
+  digitalWrite(RST_OLED, HIGH);   
+
+  // Iniciamos OLED
+  Serial.println("Iniciando pantalla...");
+  display.init();
+  display.flipScreenVertically();
+  
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(128/2, 64/2-16/2, "Iniciando...");
+  display.display();
+  delay(3000);
 }
 
 void loop() {
@@ -43,6 +63,16 @@ void loop() {
   Serial.println(" %");
   
   Serial.println();
+
+  // Actualizamos pantalla
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 10, "  TEMP     HUM");
+  char text[32];
+  sprintf(text, " %.1f°C    %.1f%%", sht30.cTemp, sht30.humidity);
+  display.drawString(0,40, text);
+  display.display();
 
   delay(5000);
 }
