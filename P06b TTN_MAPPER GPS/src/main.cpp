4c9119b6e8f8eced8c8a4bcb09041eba79f0c9bb
4m1g0 <dev.4m1g0@gmail.com>
2021-10-21 20:22:16 +0200

    Fixed P06b keys and add payload decodder

--- P06b TTN_MAPPER GPS/src/main.cpp
+++ P06b TTN_MAPPER GPS/src/main.cpp
@@ -15,15 +15,46 @@
   TX  ----- PIN 13
   RX  ----- PIN 12
   PPS No conectar
+
+  Crea un dispositivo en TTN y rellena los campos de abajo con
+  las claves correspondientes
+
+  # PAYLOAD FORMATER para TTN #
+
+  function Decoder(bytes, port) {
+    var decoded = {};
+
+    decoded.latitude = ((bytes[0]<<16)>>>0) + ((bytes[1]<<8)>>>0) + bytes[2];
+    decoded.latitude = (decoded.latitude / 16777215.0 * 180) - 90;
+  
+    decoded.longitude = ((bytes[3]<<16)>>>0) + ((bytes[4]<<8)>>>0) + bytes[5];
+    decoded.longitude = (decoded.longitude / 16777215.0 * 360) - 180;
+  
+    var altValue = ((bytes[6]<<8)>>>0) + bytes[7];
+    var sign = bytes[6] & (1 << 7);
+    if(sign)
+    {
+        decoded.altitude = 0xFFFF0000 | altValue;
+    }
+    else
+    {
+        decoded.altitude = altValue;
+    }
+  
+    decoded.hdop = bytes[8] / 10.0;
+
+    return decoded;
+  }
+
 *******************************************************************************/
 #include <Arduino.h>
 #include <TTN_esp32.h> //https://github.com/rgot-org/TheThingsNetwork_esp32
 #include "gps.h"
 #include "oled.h"
 
-const char* devEui = "XXXXXXXXXXXXXXXX"; // Rellena con el Device EUI de TTN
-const char* appEui = "XXXXXXXXXXXXXXXX"; // Rellena con el Application EUI de TTN
-const char* appKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Rellena con la Application Key de TTN
+const char *devAddr = "XXXXXXX";                          // Rellena con el Device Address de TTN
+const char *nwkSKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Rellena con la Network Session Key de TTN
+const char *appSKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Rellena con la Application Session Key de TTN
 
 TTN_esp32 ttn;
 uint8_t txBuffer[9];
